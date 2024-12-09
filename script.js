aeb73fc064c17dbf839f6a21b8e3c1eeb6ea4665
almajacob <almajacob7@gmail.com>
2024-12-09 13:33:22 +0530

    added

---
+++ script.js
@@ -0,0 +1,91 @@
+
+// alert('heyyy')
+
+const { FindOperators } = require("mongodb")
+
+// a='everyone'
+// console.log('hello to',a);
+// alert(a)
+
+// var keyword
+
+// var a=20
+// var a='heloo'
+// console.log(a);
+
+// let keyword
+
+// let a=20
+//  a='hello'
+//  console.log(a);
+
+//  const keyword
+
+// const a='mng'
+// console.log(a);
+
+// datatype
+// -------------
+
+// 1. Number datatype
+// let a
+// a=10
+// console.log(typeof a);
+// console.log(a);
+
+// 2. string datatype
+// let a
+// a='hello'
+// console.log(typeof a);
+// console.log(a);
+
+// 3. object datatype
+// let a
+// a=[1,3,6]
+// console.log(typeof a);
+// console.log(a);
+
+// 4. undefined datatype
+// let a
+// a
+// console.log(typeof a);
+// console.log(a);
+
+
+// operators
+// -------------------
+
+// Arithemetic operator
+// -,+,*,/,%,**,++,--
+// eg:
+// let a=5
+// a++
+// console.log(a);
+
+// Assignmet opertor
+// =,+=,-=,*=,/=,%=
+//  a-=10
+// console.log(a);
+
+// comparison operator
+// ==,!=,<,>,<=,>=,===,!==
+
+// eg1:
+// let a=10
+// let b='10'
+// console.log(a===b);
+
+// eg2:
+// let a=10
+// let b='10'
+// console.log(a!==b);
+
+// logical operators
+// &&(and),||(or)
+
+// let a=10
+// let b=20
+// console.log(a!=b && a==10)
+// console.log(a!=b || a==10)
+
+// here we dont use bitwise operator
