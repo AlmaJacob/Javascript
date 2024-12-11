91981798b9906bf41b2b1793fdecf7fc88b57a95
almajacob <almajacob7@gmail.com>
2024-12-11 13:32:33 +0530

    added

---
+++ loop.js
@@ -0,0 +1,92 @@
+// while loop
+
+// let i=1
+// while(i<=10){
+//     console.log(i);
+//     i++
+// }
+
+// for loop
+
+// for(let i=1;i<=15;i++){
+//     console.log(i)
+// }
+
+// do-while loop
+
+// let i=1
+// do{
+//     console.log(i)
+//     i++
+// }
+// while(i<=13)
+
+// finding given no is odd or even
+
+// function number(){
+//     let no1=parseInt(document.getElementById("no1").value)
+//     let no2=parseInt(document.getElementById("no2").value)
+//     for(let i=no1;i<=no2;i++){
+//         if (i%2!=0){
+//             console.log(i)
+//         }
+//     }
+// }
+
+// multiplication table
+
+// # a=1
+// # for i in range(a,b+1):
+
+// #     a+=1
+
+
+// function mul(){
+//     let num=parseInt(document.getElementById("num").value)
+//     a=1
+//     for(let i=a;i<=11;i++){
+//     console.log(a,'*',num,'=',a*num)
+//     a+=1
+
+// }  
+// }  
+
+
+// do_while
+
+// function mul2(){
+//     let num1=parseInt(document.getElementById("num1").value)
+//     let i=1
+//     do{
+//         console.log(i, '*',num1,'=',i*num1)
+//     i++
+
+//     }
+//     while(i<=10)
+
+// }
+
+
+# a=int(input("enter first no"))
+# b=int(input("enter last no"))
+# sum=0
+# while a<=b:
+#     sum=sum+a
+#     if a%2==0:
+#         print(a)
+#     a+=1 
+# print("sum is : ",sum) 
+
+
+ function number(){
+        let a=parseInt(document.getElementById("a").value)
+        let b=parseInt(document.getElementById("b").value)
+        while(a<=b){
+            sum=sum+a
+            if a%2==0{
+                console.log(a)
+            a+=1
+            console.log('')    
+            }
+        }
+}
